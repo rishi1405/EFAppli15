b1f063d73f287612f4f6f8bdea61b61fd5a9d09c
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 14:57:10 +0530

    EFAppli15

---
+++ EFAppli15/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EFAppli15.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Assign WCF Course to Mike" />
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Remove John from SQL Server Course" />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ EFAppli15/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EFAppli15
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+
+            GridView1.DataSource = (from student in employeeDBContext.Students
+                                    from studentCourse in student.StudentCourses
+                                    select new
+                                    {
+                                        StudentName = student.StudentName,
+                                        CourseName = studentCourse.Cours.CourseName,
+                                        EnrolledDate = studentCourse.EnrolledDate
+                                    }).ToList();
+
+            // The above query can also be written as shown below
+            //GridView1.DataSource = (from course in employeeDBContext.Courses
+
+
+            //                        from studentCourse in course.StudentCourses
+            //                        select new
+            //                        {
+            //                            StudentName = studentCourse.Student.StudentName,
+            //                            CourseName = course.CourseName,
+            //                            EnrolledDate = studentCourse.EnrolledDate
+            //                        }).ToList();
+
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            employeeDBContext.StudentCourses.Add
+                (new StudentCourse
+                {
+                    StudentID = 1,
+                    CourseID = 4,
+                    EnrolledDate = DateTime.Now
+                });
+            employeeDBContext.SaveChanges();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            StudentCourse studentCourseToRemove = employeeDBContext.StudentCourses
+                .FirstOrDefault(x => x.StudentID == 2 && x.CourseID == 3);
+            employeeDBContext.StudentCourses.Remove(studentCourseToRemove);
+            employeeDBContext.SaveChanges();
+        }
+    }
+}
